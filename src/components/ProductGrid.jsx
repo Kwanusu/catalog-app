4285dab8e43493ded7ae60b60f89f8b16245625c
Kwanusu <kwanusujoseph@gmail.com>
2026-02-13 23:35:47 +0300

    refactor product grid for spinner animation

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,8 +1,19 @@
 import { ProductCard } from "./ProductCard";
+import { LoadingSpinner } from "./ui/spinner";
 
 export const ProductGrid = ({products, loading}) => {
-    if (loading) return <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">Loading</div>
-
+    if (loading)  {
+        return <LoadingSpinner/>;
+    }
+    if (products.length === 0) {
+        return (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        )
+    };
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
       {products.map((product) => (
